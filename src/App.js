411583af1879955cfabc6f433fce07c51627082e
anniechakraborty <the.demented.genius29@gmail.com>
2023-10-21 02:43:43 +0530

    created about component with light mode and dark mode toggle using state variables.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
 
@@ -8,6 +9,7 @@ function App() {
       <NavBar title={"TextUtils"} aboutText="About Us"/>
       <div className="container my-4">
         <TextForm heading="Enter text below to analyse" />
+        <About/>
       </div>
     </>
   );
